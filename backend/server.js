5f2df7a845836d4060f8dd27f1c8aa3c5e2d597a
Rahul <rahulmhto23@gmail.com>
2025-05-24 13:59:48 +0530

    correcting error

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,24 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = [
+  "https://bhilai-project-dddg.vercel.app",
+  "https://bhilai-project.vercel.app",
+  "http://localhost:7000", // for local dev
+];
+
 const corsOptions = {
-  origin: "https://bhilai-project-22yi-qcn0kkhbx-rahul-kumar-mahtos-projects.vercel.app",
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
+
 app.use(cors(corsOptions));
 
 
@@ -29,6 +42,10 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something went wrong!");
 });
+
+app.get("/",(req,res)=>{
+  res.send("hello");
+})
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
